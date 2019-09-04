2010cf31a0704548dfc5781c5ba1fe809353d66d
kaushikdasroy <kaushikdasroy@gmail.com>
2019-09-04 12:46:46 +0000

    commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello there devops!!!\n");
+  response.end("Hello there sales folks !!! i need coffee \n");
 });
 
 // listen on localhost:8000
