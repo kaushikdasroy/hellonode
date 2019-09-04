23cc0b0a81b194aaa45c88de4ac480d2c68512be
kaushikdasroy <kaushikdasroy@gmail.com>
2019-09-04 14:13:27 +0000

    updated nodejs file

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello there devops !!  \n");
+  response.end("Hello there devops !! Lets do continuous delivery !! \n");
 });
 
 // listen on localhost:8000
